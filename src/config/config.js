5d0b96be95663872ad254a8de8bb7d9c866c7cfd
eike3237 <eike3237@users.noreply.github.com>
2020-10-27 19:53:10 -0300

    alterações feitas para utilizar na nuvem e proteger dados do BD

--- src/config/config.js
+++ src/config/config.js
@@ -2,18 +2,22 @@
 module.exports = {
     development: {
         database: {
-            host: 'localhost',
-            port: 3306,
-            name: 'db_usuario',
-            dialect: 'mysql',
-            user: 'root',
-            password: 'Eike32374847@' //Tirar a senha na hra de subir pro Git, mas coloca-la ao executa-lo
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD //Tirar a senha na hra de subir pro Git, mas coloca-la ao executa-lo
         }
     },
     production:{
         database: {
             host: process.env.DB_HOST,
-            host: process.env.DB_PORT
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
         }
     }
 }
